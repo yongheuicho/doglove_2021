6d154a0668ae87407e4b636d23153d1469f13efa
yongheuicho <yongheuicho@gmail.com>
2021-05-26 11:10:43 +0900

    add fontawesome icons

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <section class="hero is-primary">
       <div class="hero-body">
         <p class="title">
-          강아지와 고양이의 동행
+          <i class="fas fa-dog"></i> 강아지와 <i class="fas fa-cat"></i> 고양이의 동행
         </p>
         <p class="subtitle">
           강아지와 고양이에 대한 모든 정보를 볼 수 있는 곳
--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "add Bulma CSS"
+git commit -m "add fontawesome icons"
 git push -u origin master
