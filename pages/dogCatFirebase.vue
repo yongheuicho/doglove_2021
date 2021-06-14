b9d538963d20782aa76db22c02666999ed99d97e
yongheuicho <yongheuicho@gmail.com>
2021-06-14 13:13:57 +0900

    add the function for reading Firebase Realtime DB in dogCatFirebase.vue

--- pages/dogCatFirebase.vue
+++ pages/dogCatFirebase.vue
@@ -12,9 +12,82 @@
 			</div>
 		</section>
 		<hr />
+		<section class="columns">
+			<div class="column">
+				<table class="table is-striped is-hoverable is-fullwidth">
+					<thead>
+						<tr>
+							<th>번호</th>
+							<th>강아지 이름</th>
+						</tr>
+					</thead>
+					<tbody>
+						<template v-for="pos in dogNames.length">
+							<tr :key="pos">
+								<td>{{ pos }}</td>
+								<td>{{ dogNames[pos - 1] }}</td>
+							</tr>
+						</template>
+					</tbody>
+				</table>
+			</div>
+			<div class="column">
+				<table class="table is-striped is-hoverable is-fullwidth">
+					<thead>
+						<tr>
+							<th>번호</th>
+							<th>고양이 이름</th>
+						</tr>
+					</thead>
+					<tbody>
+						<template v-for="pos in catNames.length">
+							<tr :key="pos">
+								<td>{{ pos }}</td>
+								<td>{{ catNames[pos - 1] }}</td>
+							</tr>
+						</template>
+					</tbody>
+				</table>
+			</div>
+		</section>
 	</div>
 </template>
 <script>
 	import fbDb from '~/plugins/firebaseDb';
-	export default {};
+	export default {
+		data() {
+			return { dogNames: [], catNames: [] };
+		},
+		mounted() {
+			// this.getFbDbNames('cats');
+			// this.getFbDbNames('dogs');
+			this.onFbDbNames('cats');
+			this.onFbDbNames('dogs');
+		},
+		methods: {
+			getFbDbNames(refName) {
+				fbDb
+					.ref(refName)
+					.child('names')
+					.get()
+					.then((snapshot) => {
+						if (snapshot.exists()) {
+							if (refName === 'cats') this.catNames = snapshot.val();
+							else if (refName === 'dogs') this.dogNames = snapshot.val();
+						}
+					});
+			},
+			onFbDbNames(refName) {
+				fbDb
+					.ref(refName)
+					.child('names')
+					.on('value', (snapshot) => {
+						if (snapshot.exists()) {
+							if (refName === 'cats') this.catNames = snapshot.val();
+							else if (refName === 'dogs') this.dogNames = snapshot.val();
+						}
+					});
+			},
+		},
+	};
 </script>
